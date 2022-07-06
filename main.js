bce2ccb3d4bfffd58aed0808406ed4a1aa7cde32
fatima2009sarah <80685244+fatima2009sarah@users.noreply.github.com>
2022-07-06 22:19:33 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+song = "";
+
+function preload(){
+    song = loadSound("music.mp3");
+}
+
+function setup(){
+    canvas = createCanvas(600 , 420);
+    canvas.center();
+    
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw(){
+    image(video , 0 , 0 , 600 , 420);
+}
+
+function play(){
+    song.play();
+}
+
+function stop(){
+    song.stop();
+}
